cc1dd53fadf3618d85083d069982995ed155a3b8
VIMAL4705 <vimaljayavel2799@gmail.com>
2025-10-30 23:01:25 +0530

    Create

---
+++ 171.Excel Sheet Column Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int titleToNumber(String s) {
+        int result = 0;
+        for(char c : s.toCharArray()){
+            int d = c - 'A' + 1;
+            result = result*26 + d;
+        }
+        return result;
+    }
+}
